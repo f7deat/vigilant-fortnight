4b4f9b8c901376c6f91952b1107707e78908db5e
f7deat <tan.dct9x@gmail.com>
2021-06-22 22:24:49 +0700

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import CryptoJS from 'crypto-js';
+
 
 function App() {
+
+  const [hashValue, setHashValue] = useState<string>('')
+
+  function handleChange(value: string) {
+    var md5Hash = CryptoJS.MD5(value);
+    setHashValue(md5Hash.toString());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <nav className="sidebar">
+        <div className="sidebar-item active">
+          <i className="fab fa-slack"></i> Transactions
+        </div>
+        <div className="sidebar-item">
+          <i className="fab fa-slack-hash"></i> MD5 Hash
+        </div>
+      </nav>
+      <div className="flex-grow">
+      <div className="h-screen">
+        <div className="heading">
+          <h1>Transactions</h1>
+          <input className="search"/>
+        </div>
+        <div>{hashValue}</div>
+        <input onChange={(e: any) => handleChange(e.target.value)} className="form-control"/>
+      </div>
+      </div>
     </div>
   );
 }
